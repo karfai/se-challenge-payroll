f1990e43f899bd9b4d4a62bb2b82c378889a452c
Don Kelly <karfai@gmail.com>
2018-06-07 09:08:25 +0400

    Update the schema based on the sample file
    - group_id should be in hours because employees can be in multiple
      groups
    - add enough data to cover employees in the sample file

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,21 +1,23 @@
 # --- !Ups
 CREATE TABLE groups (
-    name varchar(255) NOT NULL PRIMARY KEY,
+    name varchar(50) NOT NULL PRIMARY KEY,
     rate decimal NOT NULL
 );
 
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    group_id varchar(255) REFERENCES groups(name)
+    name varchar(255) NOT NULL
 );
 
 CREATE TABLE hours (
+    time_sheet_id varchar(50),
     date date NOT NULL,
     hours decimal NOT NULL,
-    employee_id int REFERENCES employees(id)
-)
+    employee_id int REFERENCES employees(id) NOT NULL,
+    group_id varchar(50) REFERENCES groups(name) NOT NULL
+);
+
 # --- !Downs
+DROP TABLE hours;
 DROP TABLE employees;
 DROP TABLE groups;
-DROP TABLE hours;
