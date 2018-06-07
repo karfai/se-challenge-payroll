bfa79fe78ffcccdc77ad413b3f1bebf3ed649985
Don Kelly <karfai@gmail.com>
2018-06-07 16:47:03 +0400

    Throw in some basic Knockout to render the page from API data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
   $('#modal-upload').modal('attach events', '#button-show-modal', 'show');
+  let vm = {
+    report: ko.observableArray([])
+  };
+
+  $.getJSON("/time_sheets", function (report) {
+    console.log(report);
+    vm.report(_.map(_.get(report, "totals"), function (o) {
+      return {
+        name: o.pay.name,
+        total: o.pay.total,
+        period: `${o.starts} - ${o.ends}`,
+        groups: _.join(o.pay.groups, ", ")
+      };
+    }));
+  });
+  
+  ko.applyBindings(vm, document.getElementById('report-contents'));
 });
